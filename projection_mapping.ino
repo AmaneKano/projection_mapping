f1c841c4e41cfe25c45df9f7ed32373b10dfd603
AmaneKano <60719485+AmaneKano@users.noreply.github.com>
2020-12-29 10:53:49 +0900

    Add files via upload

---
+++ projection_mapping.ino
@@ -0,0 +1,18 @@
+int digitalPin = 2;
+int val = LOW;
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(digitalPin, INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  val = digitalRead(digitalPin);
+  if(val==HIGH){
+    Serial.print(val);
+    Serial.print("\n");
+    Serial.read();
+    delay(100);
+  }
+}
